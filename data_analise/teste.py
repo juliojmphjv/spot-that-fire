739f5a2a23039dc3ed1721fd6cc9601a773d8909
Felipe Guimaraes <felipeguimaraes1@Felipes-MacBook-Pro.local>
2019-10-19 20:07:26 -0300

    works

--- data_analise/teste.py
+++ data_analise/teste.py
@@ -7,17 +7,20 @@ import urllib
 
 def read_data():
 
+    def num_missing(x):
+        return sum(x.isnull())
+
     lonW = -73.99  # Longitude Oeste.
     lonE = -33.86  # Longitude Leste.
     latS = -28.63  # Latitude Sul.
     latN = 5.29  # Latitude Norte.
     dgrad = .25  # Resolução do píxel.
 
-    df = pd.read_csv('datawarehouse/focos.csv')
+    df = pd.read_csv('focos.csv')
 
+    states = df.groupby(['estado']).ngroups
 
-
-    print(df)
+    print(states)
 
 
 read_data()
