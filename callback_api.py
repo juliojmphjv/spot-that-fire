2fbb468fd8005805ceeb0d95e4b855f65a5fca12
Julio Nascimento <jnascimento@gatefy.com>
2019-10-20 13:24:36 -0300

    Change host flask

--- callback_api.py
+++ callback_api.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 from chatterbot import ChatBot
@@ -8,28 +7,33 @@ import time
 
 app = Flask(__name__)
 
+
 def cadastro(nome, telefone, email, cep, cidade, iptu):
     pass
 
+
 def salutation(msg):
 
-    if msg == '1':
-        return ''
+    if msg == "1":
+        return ""
 
-    if msg == '2':
-        return 'Reportar'
+    if msg == "2":
+        return "Reportar"
     else:
-        return 'Olá, eu sou a Abellis, uma inteligencia artificial. Envie *1* para se cadastrar em nosso sistema para receber notificações de incêndios florestais e relatórios. ' \
-              'Ou envie *2* para reportar um incêndio.'
+        return (
+            "Olá, eu sou a Abellis, uma inteligencia artificial. Envie *1* para se cadastrar em nosso sistema para receber notificações de incêndios florestais e relatórios. "
+            "Ou envie *2* para reportar um incêndio."
+        )
 
-@app.route("/sms", methods=['GET', 'POST'])
+
+@app.route("/sms", methods=["GET", "POST"])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
     resp = MessagingResponse()
 
-    bd = request.form['Body']
-    fr = request.form['From']
+    bd = request.form["Body"]
+    fr = request.form["From"]
 
     resposta = salutation(bd)
     # if bd == '1':
@@ -41,25 +45,34 @@ def sms_reply():
     send = MessagingResponse()
     send.message(resposta)
 
-
-
     return str(send)
+
 
 def replier(msg):
 
     resp = MessagingResponse()
     resp.message(msg)
 
+
 def train_smart_bot():
 
-    bot = ChatBot(name='Abellis', read_only=False, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
+    bot = ChatBot(
+        name="Abellis",
+        read_only=False,
+        logic_adapters=[
+            "chatterbot.logic.MathematicalEvaluation",
+            "chatterbot.logic.BestMatch",
+        ],
+    )
     list_trainer = ListTrainer(bot)
-    for item in (talks_data.small_talk, talks_data.math_talk_1, talks_data.math_talk_2):
+    for item in (
+        talks_data.small_talk,
+        talks_data.math_talk_1,
+        talks_data.math_talk_2,
+    ):
         list_trainer.train(item)
-
 
 
 if __name__ == "__main__":
 
-    app.run(debug=True, port=4000)
-
+    app.run(debug=True, host="0.0.0.0", port=4000)
