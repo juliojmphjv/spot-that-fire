ff0af2a583470eb08413653713187a844e28fc83
Julio Nascimento <jnascimento@gatefy.com>
2019-10-20 05:37:18 -0300

    Fix remove old things2

--- app/settings.py
+++ app/settings.py
@@ -122,11 +122,3 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ]
-}
