ee421893887b0bc1b7be6f3c94c449cbc15d3749
Julia Yumi Nishihiro <juliayn3@gmail.com>
2019-10-20 00:50:56 -0300

    troca logo para nome, vice-versa

--- pages/urls.py
+++ pages/urls.py
@@ -5,13 +5,10 @@ from .views import homePageView, home, boilerplate, whats_signup, cadastro, mapa
 urlpatterns = [
     path("home/", home, name="home"),
     path("", home, name="home"),
-    path("boilerplate/", boilerplate, name="home"),
     path("whatsapp/", whats_signup, name="whats"),
     path("cadastro/", cadastro, name="cadastro"),
-    path("home/cadastro/", cadastro, name="cadastro"),
     path("map", mapa, name="mapa"),
-    path("home/map", mapa, name="mapa"),
     path("missao", missao, name="missao"),
-    path("home/missao", missao, name="missao")
+
 
 ]
